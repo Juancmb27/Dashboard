209810e6edb58e60f35edcc59c1bb3cdd6e9e700
juancmb27 <juanmontealegreb.27@gmail.com>
2023-08-08 17:04:21 -0500

    Commits

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react' // Hooks
 
 import './App.css';
 
-
+//Se importan los componentes principales de la app
 import Header from './Components/Header'
 import CardPrincipal from './Components/CardPrincipal';
 import Card from './Components/Card'
@@ -11,47 +11,53 @@ import TableCoins from './Components/TableCoins'
 import Footer from './Components/Footer'
 
 function App() {
- 
+  /* 3 estados principales. setCoins en donde almacena el valor de todas las monedas en el consumo de la api.
+  setCurrency, almacena el valor de todas las divisas que entrega el cosnumo de la api.
+  setSelCur, almacena el valor de la divisa seleccionada. */
+
   const [coins, setCoins] = useState();
   const [currency, setCurrency] = useState();
   const [selCur, setSelCur] = useState("usd");
 
- 
+// Se realiza el cosnumo de la api por metodo async await y usando fetch
+// Se define la variable getData que contiene el llamado de la api.
+
   const getData = async () => {
-    
+
     const response = await fetch(
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${selCur}&order=market_cap_desc&per_page=4&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C90d%2C1y`
     );
-
-    
+/* 
+      Cuando hacemos el llamado de la API el valor que nos retorna es un arreglo de objetos tipo json, este valor se almacena en una variable y lo vamos a leer por medio del método json()
+    */
     const json = await response.json();
-    
-   
+
+// ULR que permite acceder a diferentes divisas y se almacena en una variable
     const response_cur = await fetch(
       `https://api.coingecko.com/api/v3/simple/supported_vs_currencies`
     );
 
-    
+
     const cur = await response_cur.json();
-    
-    
+
+// Almacena toda la info de de las monedas en ese estado.
     setCoins(json);
 
-    
+// Almacena el valor de las divisas en ese estado.
     setCurrency(cur);
   };
 
-  
+// Estado que carga la información cuando el componente esta listo.
   useEffect(() => {
     getData();
   }, []);
 
-  
+// Estado que que carga la información cuando seleccionamos otra divisa.
   useEffect(() => {
     getData();
   }, [selCur]);
 
-  
+
   return !coins ? (
     "Cargando..."
   ) : (
@@ -100,17 +106,17 @@ function App() {
 
 export default App;
 
-
+// La función para formatear los decimales 
 export function deleteDec(val, decimal) {
   return val.toFixed(decimal);
 }
 
-
+// Función para asignar color de acuerdo al valor numerico.
 export function colorDec(num) {
   return num > 0 ? "green" : "red";
 }
 
-
+// Función para separar los valores en miles. 
 export const numberF = Intl.NumberFormat("es-ES");
 
 
